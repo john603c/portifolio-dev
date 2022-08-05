e15f2906c0a012dd37661846762ade8c2821bddf
bigjohn <john603c@hotmail.com>
2022-08-05 18:06:36 -0300

    inicio home-page

---
+++ skin/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#skills-banners .banner-list").slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+});
