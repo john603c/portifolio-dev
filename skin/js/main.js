433e9083a56f38c25bf4e81552b649a4f8a4546a
bigjohn <john603c@hotmail.com>
2022-08-05 22:09:45 -0300

    continuando a home-page

--- skin/js/main.js
+++ skin/js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-    $("#skills-banners .banner-list").slick({
+
+    $("#header").addClass("closed");
+
+    $(".close-menu i").click(function(){
+        $("#header").addClass("closed");
+        $("#main-mask").removeClass("active");
+    });
+
+    $("#menu-icon").click(function(){
+        $("#header").addClass("open").removeClass("closed");
+        $("#main-mask").addClass("active");
+    });
+
+    $("#skills-container .banner-list").slick({
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 4,
@@ -8,4 +21,22 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 5000,
     });
+
+    $("#exp-container .exp-list li").hover(function(){
+        $(this).toggleClass("animate__animated animate__rubberBand")
+    });
+
+    $(window).scroll(function() {
+		if ($(this).scrollTop()) {
+			$('.backTop').fadeIn();
+            $('#menu-icon').fadeIn();
+		} else {
+			$('.backTop').fadeOut();
+            $('#menu-icon').fadeOut();
+		}
+	});
+
+	$(".backTop").click(function () {
+	   $("html, body").animate({scrollTop: 0}, 100);
+	});
 });
